8a30992fcd3610c1da9c684624739f65ae84652a
eojiinn <olbloe@kakao.com>
2025-02-12 10:38:53 +0900

    websocket 제거

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from app.models import db, User
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
-from flask_socketio import SocketIO
+# from flask_socketio import SocketIO
 
-socketio = SocketIO()
+# socketio = SocketIO()
 login_manager = LoginManager()
 migrate = Migrate()
 csrf = CSRFProtect()
@@ -18,7 +18,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-    socketio.init_app(app)
+    # socketio.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app import create_app, socketio
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(port=5000, host='0.0.0.0', debug=True)
-    socketio.run(app, port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
+    # socketio.run(app, port=5000, host='0.0.0.0', debug=True)
