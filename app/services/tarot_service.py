3a5ac1acb41b3af1f4584cafebcfc51736d04584
eojiinn <djwls2726@gmail.com>
2025-02-28 03:09:59 +0900

    수정: API 수정에 따른 함수 수정(추가, 분리), process_query 함수 로직 수정

--- app/services/tarot_service.py
+++ app/services/tarot_service.py
@@ -7,7 +7,7 @@ from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.schema.runnable import RunnableSequence  
 from langchain.schema import StrOutputParser 
-from app.data import tarot_meaning, templates
+from app.data import tarot_meaning, templates, tarot_card_images
 
 
 # 환경변수 로드
@@ -15,6 +15,8 @@ load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 # 타로 의미 데이터 로드
 TAROT_CARD_MEANINGS = tarot_meaning.TAROT_CARD_MEANINGS
+# 타로 카드 이미지 URL
+TAROT_CARD_IMAGES = tarot_card_images.TAROT_CARD_IMAGES
 
 
 class TarotReader:
@@ -82,34 +84,59 @@ class TarotReader:
             ]
         )
 
+    
+    def topic_update(self, topic):
+        """
+        ✅ 토픽 저장
+        """
+        self.conversation_state["topic"] = topic
+
+        return self.conversation_state["topic"]
 
     def draw_tarot_cards(self):
         """
         ✅ 22장의 메이저 아르카나 타로 카드 중 3장 무작위 뽑기
         """
-        cards = [
+        tarot_cards = [
             "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant",
             "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man",
             "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"
         ]
-        return random.sample(cards, 3)
+        # 카드 정보 업데이트
+        self.conversation_state["cards"] = random.sample(tarot_cards, 3)
+        # 카드 뽑음 여부 True
+        # self.conversation_state["is_card_drawn"] = True
 
+        return self.conversation_state["cards"]  # ["카드이름"]
+
+
+    def card_images_url(self, cards):
+        """
+        ✅ self.conversation_state["cards"]에 든 카드 3장의 이미지 URL을 반환
+        """
+        
+        return {card: TAROT_CARD_IMAGES.get(card, "https://i.namu.wiki/i/BldqgVbeK4tGO4_VXDjy-3cMuC_Zw7WDqDUDUsApMzkry4X4SamqFtv6FmL872EJq8uQTelygkk8uG34o1H-4Q.webp") for card in cards}
+        
 
     def card_keywords(self, cards):
         """
         ✅ 뽑은 카드의 의미를 담는 함수
+        - process_query()에서 사용
 
         Args : cards (list) 뽑힌 카드 리스트
         Return : dict
         """
-        return {card: TAROT_CARD_MEANINGS[card] for card in cards}
+        # 카드 키워드 업데이트
+        self.conversation_state["card_keywords"] = {card: TAROT_CARD_MEANINGS[card] for card in cards}
+
+        return self.conversation_state
 
 
-    def process_query(self, text, topic=None):
+    def process_query(self, text):
         """
         ✅ 사용자의 질문을 처리하고 대화 응답을 chunk단위로 반환하는 함수
 
-        - 첫 리딩이면 카드를 뽑고 카드 키워드 저장
+        - 첫 리딩이면 뽑은 카드로 카드 키워드 저장장
         - 후속 질문이면 기존 카드 정보를 유지하면서 답변 제공
 
         Args : 
@@ -119,40 +146,35 @@ class TarotReader:
             str : 모델이 생성한 타로 해석 (stream)
         """
 
-        # 사용자 질문 저장
+        # 1. 사용자 질문 저장
         self.memory.chat_memory.add_user_message(text)
 
-        # 프롬프트 생성
+        # 2-1. 첫 리딩이면 : 고른 토픽과 뽑은 카드가 이미 저장된 상태
         if not self.conversation_state["is_card_drawn"]:
-            # 첫 번째 리딩: 카드 뽑기
-            cards = self.draw_tarot_cards()
-            self.conversation_state.update({
-                "cards": ', '.join(cards),
-                "topic": topic,
-                "is_card_drawn": True,
-                "card_keywords": self.card_keywords(cards),
-            })
-            
+            # 카드 키워드 저장
+            self.card_keywords(self.conversation_state["cards"])
+            # is_drawn 업데이트 추가
+            # 프롬프트 생성
             prompt_template = self.create_prompt(is_first_reading=True)
+        # 2-2. 후속 리딩이면
         else:
-            # 후속 질문
             prompt_template = self.create_prompt(is_first_reading=False)
 
-        # 모델 실행
+        # 3. 모델 실행
         chain = RunnableSequence(
             prompt_template, 
             self.model,      
             StrOutputParser()  
         )
 
-        # 응답 streaming
+        # 4. 응답 streaming
         full_response = ""
         for chunk in chain.stream(input={
             "text": text,
-            "topic": topic,
-            "chat_history": self.memory.chat_memory.messages,
+            "topic": self.conversation_state["topic"],
             "cards": self.conversation_state["cards"],
-            "card_keywords": self.conversation_state["card_keywords"]
+            "card_keywords": self.conversation_state["card_keywords"],
+            "chat_history": self.memory.chat_memory.messages,
         }):
             yield chunk   
             time.sleep(0.1)
