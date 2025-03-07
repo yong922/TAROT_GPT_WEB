21619713b52bbb7d733a8d49311f7f5414e8237e
eojiinn <djwls2726@gmail.com>
2025-03-07 21:13:47 +0900

    수정: chat에서 chat_history 분리

--- app/__init__.py
+++ app/__init__.py
@@ -24,9 +24,9 @@ def create_app():
     def load_user(user_id):
         return User.query.get(user_id)
     
-    from app.routes import auth_bp, chat_bp, history_bp
+    from app.routes import auth_bp, chat_bp, chat_history_bp
     app.register_blueprint(auth_bp, url_prefix='/')
     app.register_blueprint(chat_bp, url_prefix='/chat')
-    app.register_blueprint(history_bp, url_prefix='/history')
+    app.register_blueprint(chat_history_bp, url_prefix='/chat_history')
 
     return app
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 auth_bp = Blueprint('main', __name__)
 chat_bp = Blueprint('chat', __name__)
-history_bp = Blueprint('history', __name__)
+chat_history_bp = Blueprint('chat_history', __name__)
 
-from . import auth_routes, chat_routes, history_routes
+from . import auth_routes, chat_history_routes, chat_routes
