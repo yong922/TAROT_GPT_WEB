7f00dc4d5cd2cdfb9cfe9729667a27f90bdeb823
eojiinn <djwls2726@gmail.com>
2025-05-12 18:20:59 +0900

    수정: 테스트용 API KEY 추가

--- app/config.py
+++ app/config.py
@@ -14,6 +14,8 @@ class Config:
     # CSRF 보호 비활성화 (Postman 테스트용)
     WTF_CSRF_ENABLED = False
 
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')  # 테스트용 더미 키
+
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # 메모리 DB를 사용하여 테스트 환경에서 데이터베이스 초기화
     TESTING = True  # Flask의 테스트 모드 활성화
