af3edca84fe0f94a03164bde1167507491dce7b1
yong922 <cucury922@naver.com>
2025-05-14 18:23:44 +0900

    25-05-14 ci 환경변수 설정 및 config, run 수정

--- run.py
+++ run.py
@@ -4,8 +4,5 @@ os.environ.setdefault("APP_ENV", "dev")
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
